5772bdea42e08198f99b9895b933a7cc79fdf1ad
Heechan-Kim-2869 <2218160@donga.ac.kr>
2025-03-12 17:01:39 +0900

    Add files via upload

---
+++ pmassign.cpp
@@ -0,0 +1,24 @@
+/* 각종 연산자와 할당, 복합할당 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int x = 2;
+
+    x += 4;
+
+    cout << "a + b: " << a + b << endl;
+    cout << "-a: " << -a << endl;
+    cout << "a * b: " << a * b << endl;
+    cout << "x += 4: " << x << endl;
+
+    b /= 10;
+    cout << b;
+
+    return 0;
+
+}*/
