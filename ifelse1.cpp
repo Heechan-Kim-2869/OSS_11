bba10833e536235ea875695630eb3d44be6cc8dc
Heechan-Kim-2869 <2218160@donga.ac.kr>
2025-03-22 23:48:40 +0900

    Add files via upload

---
+++ ifelse1.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int score;
+
+    cout << "0~100사이의 점수 입력: ";
+    cin >> score;
+
+    if(score >= 70)
+    {
+        cout << "PASS" << endl;
+    }
+    else
+    {
+        cout << "FAIL" << endl;
+    }
+    return 0;
+}
+    */
