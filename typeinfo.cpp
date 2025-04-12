e6193d4615fd237a12585c6224e452059cd36b5f
Heechan-Kim-2869 <2218160@donga.ac.kr>
2025-04-12 10:00:09 +0900

    Add files via upload

---
+++ typeinfo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "iu.h"
+using namespace std;
+
+int main()
+{
+    double a = 1;
+    cout << showpoint << a << endl;
+    cout << iu::cout << endl;
+
+    return 0;
+}
