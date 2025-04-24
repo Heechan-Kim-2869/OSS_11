11f7dd10826f9031886995ef8c8630c9c7474e05
Heechan-Kim-2869 <2218160@donga.ac.kr>
2025-04-24 14:03:43 +0900

    Add files via upload

---
+++ sorry.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string sorry;
+    cout << "김희찬이 잘못했습니다: ";
+    cin >> noskipws >> sorry;
+
+    cout << "이제 앞으로" << endl;
+    cout << "더 친해질 일 밖에" << endl;
+    cout << "안 남았다는 말이었어." << endl;
+    cout << "잘못했습니다." << sorry << endl;
+
+    return 0;
+}
